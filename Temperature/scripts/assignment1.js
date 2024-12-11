186901d1f5731c2c71a4cf0c5ace19f24a7b9b9f
Brandon Merle <brandon.merle@yahoo.com>
2024-12-11 17:10:42 -0500

    Add files via upload

---
+++ Temperature/scripts/assignment1.js
@@ -0,0 +1,25 @@
+console.log("Script");
+
+let character = "Surfer";
+let surfterm = "Cowabunga";
+let animal = "sea gull";
+let food = "peanut";
+let bodypart = "head";
+
+
+const place = "ocean";
+
+console.log("---A Funny Surf Tale---")
+
+console.log("A " + character + " paddled out into the " + place + ", ready to shred. As he caught a wave, he yelled, " + surfterm + "! and promptly fell off his board. A " + animal + " swooped up, mistaking his " + bodypart + " for a particularly large " + food + ". The man, covered in seaweed, started laughing.");
+
+console.log("--- Different Version of the Story ---");
+
+character = "Plumber";
+surfterm = "Hang ten";
+animal = "dolphin";
+food = "cucumber";
+bodypart = "leg";
+
+
+console.log("A " + character + " paddled out into the " + place + ", ready to shred. As he caught a wave, he yelled, " + surfterm + "! and promptly fell off his board. A " + animal + " swooped up, mistaking his " + bodypart + " for a particularly large " + food + ". The man, covered in seaweed, started crying.");
---
+++ Temperature/scripts/script.js
@@ -0,0 +1,41 @@
+console.log("Script");
+
+//string "" a sequence of characters
+let myName = "Merle";
+let firstName = "Brandon";
+
+//numbers
+let myAge = 41;
+let myShoe = 11;
+
+//boolean 
+let isStudent = true; //true or false
+
+//display
+console.log(firstName)
+console.log(myAge)
+
+//concatenation
+console.log("My name is " + firstName + " " + myName + " and I am " +myAge + " and my shoe size is " + myShoe) 
+
+//operations 
+let num1 = 10;
+let num2 = 20;
+
+let addition = num1 + num2;
+let sub = num1 - num2;
+let mult = num1 * num2;
+let division = num1 / num2; //can also be div for short
+
+console.log("addition is: " + addition);
+console.log("subtraction is: " + sub);
+console.log("multiplicaiton is: " + mult);
+console.log("division is: " + division)
+
+let radius = prompt ('Enter the radius'); 
+const pi = 3.1416;
+let area = pi * radius * radius;
+
+console.log("The area of a circle with a radius of "+radius + " is " +area);
+
+const weekDays = 5;
