b45ae1d2d8b6dcd2fc204d8346825edacf85f81c
Brandon Merle <brandon.merle@yahoo.com>
2024-12-17 19:58:08 -0500

    Add files via upload

---
+++ Temperature/scripts/assignment3.js
@@ -0,0 +1,16 @@
+
+function convertTemp (){
+
+    let tmp = prompt("Enter the temperature");
+    let scale = prompt("Enter F or C");
+    let Answer = 0;
+
+    
+    let result;
+    if(scale= "F" ){
+        Answer = (tmp * 9/5) + 32;
+    }else{
+        Answer = (tmp - 32) * 5/9;
+         }
+    document.getElementById("result").innerHTML="The converted temperature is: " + result;
+        }
